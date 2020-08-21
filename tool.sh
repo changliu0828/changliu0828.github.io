58301074a3217755173225dc5b8c4372d79a1073
ChangLiu <changliu0828@gmail.com>
2020-08-21 10:39:21 +0800

    update

--- tool.sh
+++ tool.sh
@@ -4,3 +4,10 @@ if [ $1 == "post" ]; then
   mkdir content/post/$2/image  
   hugo new post/$2/$2.md
 fi
+if [ $1 == "update" ]; then
+  rm -r docs/*
+  hugo -d docs
+  git add .
+  git commit -m "update"
+  git push origin master
+fi
