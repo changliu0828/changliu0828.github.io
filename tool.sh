f6802da4fc851b4cd1c2408c3fb7c493c9c66845
ChangLiu <changliu0828@gmail.com>
2020-09-09 16:38:04 +0800

    update

--- tool.sh
+++ tool.sh
@@ -2,11 +2,12 @@
 if [ $1 == "post" ]; then
   mkdir static/image/$2
   hugo new post/$2.md
-fi
-if [ $1 == "update" ]; then
+elif [ $1 == "update" ]; then
   find docs/* | grep -v "CNAME" | xargs rm -r
   hugo -d docs
   git add .
   git commit -m "update"
   git push origin master
+else
+  echo "err command:" $1
 fi
